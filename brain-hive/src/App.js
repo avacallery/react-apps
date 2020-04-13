921ed11d57034f0852dab5eeefc617cf0dc82d73
avacallery <avacallery@gmail.com>
2020-04-13 11:53:35 -0400

    added renderPosts method to App.js

--- brain-hive/src/App.js
+++ brain-hive/src/App.js
@@ -1,42 +1,23 @@
 import React, { Component } from 'react';
 import Post from './components/Post'
-//{ } extracts component property 
-
 import './App.css';
-
-//turn functional component into class-based component 
-
-//App will (inherit...extends) gain all the properties of Component 
-//that's how we're going to get props/state/mounting etc 
+import posts from './mock/posts';
 
 class App extends Component {
-  //now this is an attribute of the App class
-  //so no const in front of post
-    post = {
-    posterName: 'Arnell Milhouse',
-    resourceAuthor: 'Albert Einstein',
-    jobSkillLevel: 'Advanced',
-    cohort: '0',
-    title: 'The Universal Equation',
-    categories: ['FORTRAN', 'redux'],
-    summary: 'This was a great article about blending fortran and redux.',
-    link: 'www.google.com/search?q=AlbertEintstein',
-    resourceType: 'Article',
-    datePublihed: '1943-10-03:00:00:00z',
-    videolength: null,
-    timeToComplete: 3,
-    rating: 5,
-    comments: [{
-        commenter: 'Cliff', text: 'Nice post.'
-    },
-    { commenter: 'Ava', text: 'Einstein rules.' }
-    ]
-}
+
+  renderPosts = () => {
+    const display = posts.map(post => {
+      //importing prop from Post component and reassigning
+      return <Post post={post} />
+    })
+    return display;
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to BrainHive!</h1>
-        <Post post={this.post}/>
+        {this.renderPosts()}
       </div>
     );
   }
